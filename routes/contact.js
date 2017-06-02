c07755dfa6ffa9d50ba4818ac0c5baa09a80601c
hrc099 <hrc099@gmail.com>
2017-06-02 13:36:36 +0200

    Final commit: Express + Jade + Nodemailer app
    App with routes and form for sending email
    
    In contact.js file user and pass variables need to be filled with real gmail address and password, also user needs to be logged in his/her gmail acc in web browser and maybe allow app to send mail

--- routes/contact.js
+++ routes/contact.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('contact', { title: 'contact' });
+});
+
+router.post('/send', function(req, res, next) {
+  var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: '',
+      pass: ''
+    }
+  });
+
+  var mailOptions = {
+    from: 'Korisnik forme',
+    to: 'hrc099@gmail.com',
+    subject: 'Website Submission',
+    text: 'Poslano s test forme...Ime: '+req.body.ime+ ' Email: '+req.body.email+ ' Poruka: '+req.body.poruka,
+    html: '<p>Poslano s test forme s podacima...</p><ul><li>Ime: '+req.body.ime+ '</li><li>Email: '+req.body.email+ '</li><li>Poruka: '+req.body.poruka+ '</li></ul>'
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if(error) {
+      console.log(error);
+      res.redirect('/');
+    } else {
+      console.log('Poruka poslana: '+info.response);
+      res.redirect('/');
+    } 
+  });
 });
 
 module.exports = router;
